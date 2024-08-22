8962901c7a2135f32835e40b1cf7145f52843604
ram <k.khalifa@gpseducation.lcl>
2024-08-22 14:00:17 +0300

    mack _targetElement methode to more clening

--- dist/scripts/components/Base.js
+++ dist/scripts/components/Base.js
@@ -4,14 +4,18 @@ export class Base {
         this._hostId = _hostId;
         this._elementId = _elementId;
         this._positionElementStart = _positionElementStart;
-        this._template = document.getElementById(this._templateId);
-        this._hostElement = document.getElementById(this._hostId);
-        const templateContent = document.importNode(this._template.content, true);
+        const [template, _] = this._targetElement(this._templateId, this._hostId);
+        const templateContent = document.importNode(template.content, true);
         this.element = templateContent.firstElementChild;
         if (this._elementId) {
             this.element.id = this._elementId;
             this._insertElement(this._positionElementStart);
         }
+    }
+    _targetElement(templateId, hostId) {
+        this._template = document.getElementById(templateId);
+        this._hostElement = document.getElementById(hostId);
+        return [this._template, this._hostElement];
     }
     _insertElement(postionStart) {
         const isInsertStart = postionStart ? "afterbegin" : "beforeend";
--- src/scripts/components/Base.ts
+++ src/scripts/components/Base.ts
@@ -1,6 +1,6 @@
 export class Base<T extends HTMLElement> {
-  private _template: HTMLTemplateElement;
-  private _hostElement: HTMLDivElement;
+  private _template!: HTMLTemplateElement;
+  private _hostElement!: HTMLDivElement;
   public element: T;
 
   constructor(
@@ -10,16 +10,16 @@ export class Base<T extends HTMLElement> {
     private _positionElementStart: boolean,
   ) {
      
-    // assign template element
-    this._template = document.getElementById(
-      this._templateId
-    )! as HTMLTemplateElement;
-    // assign host element
-    this._hostElement = document.getElementById(this._hostId)! as HTMLDivElement;
-    // import template content
+    // // assign template element
+    // this._template = document.getElementById(
+    //   this._templateId
+    // )! as HTMLTemplateElement;
+    // // assign host element
+    // this._hostElement = document.getElementById(this._hostId)! as HTMLDivElement;
+    // // import template content
 
-
-    const templateContent = document.importNode(this._template.content, true);
+const[template,_]=this._targetElement(this._templateId,this._hostId)
+    const templateContent = document.importNode(template.content, true);
     this.element = templateContent.firstElementChild! as T;
 
     if (this._elementId) {
@@ -32,9 +32,21 @@ export class Base<T extends HTMLElement> {
  * @desc target elements : templtae ,host 
  * @param1 templeateid 
  * @param2 hostid 
- * @returns template , host
+ * @returns [template , host]
  */
 
+private _targetElement(templateId:string,hostId:string):[HTMLTemplateElement,HTMLDivElement]{
+ // assign template element
+ this._template = document.getElementById(
+  templateId
+)! as HTMLTemplateElement;
+// assign host element
+this._hostElement = document.getElementById(hostId)! as HTMLDivElement;
+// import template content
+
+
+return [this._template,this._hostElement]
+}
 
 
 private _insertElement(postionStart: boolean) {
